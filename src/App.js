c69a66cc4a0ef1be518b7ebac3818e21f13c3731
Matt Salmons <mattcsalmons@gmail.com>
2021-11-03 10:43:31 -0600

    writes search filter method

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
   }
 
   onSearchChange(e) {
-    console.log(e.target.value);
     this.setState({ searchField: e.target.value })
   }
 
@@ -25,10 +24,12 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
         <input type='search' placeholder='Search monsters' onChange={ this.onSearchChange }/>
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
